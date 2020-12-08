9dd0f2f67af20cddf8322791221631a173746879
Azhar <azharthegreat123@gmail.com>
2020-12-08 17:29:27 +0530

    removing few line related to default method

--- post.py
+++ post.py
@@ -6,12 +6,6 @@ incomes = [
   { 'description': 'salary', 'amount': 5000 }
 ]
 
-
-@app.route('/incomes')
-def get_incomes():
-  return jsonify(incomes)
-
-
 @app.route('/incomes', methods=['POST'])
 def add_income():
   incomes.append(request.get_json())
