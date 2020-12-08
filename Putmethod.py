8efdd90ad0e991a57ea3bb843c3c227af0b429b0
Azhar <azharthegreat123@gmail.com>
2020-12-08 12:32:49 +0530

    removing unnecessary method

--- Putmethod.py
+++ Putmethod.py
@@ -23,29 +23,9 @@ tasks = [
 def get_tasks():
     return jsonify({'tasks': tasks})
     
-@app.route('/<int:task_id>', methods=['GET'])
-def get_task(task_id):
-    task = [task for task in tasks if task['id'] == task_id]
-    if len(task) == 0:
-        abort(404)
-    return jsonify({'task': task[0]})
-
 @app.errorhandler(404)
 def not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
-
-@app.route('/', methods=['POST'])
-def create_task():
-    if not request.json or not 'title' in request.json:
-        abort(400)
-    task = {
-        'id': tasks[-1]['id'] + 1,
-        'title': request.json['title'],
-        'description': request.json.get('description'),
-        'done': request.json.get('done')
-    }
-    tasks.append(task)
-    return jsonify({'task': task}), 201
 
 @app.route('/<int:task_id>', methods=['PUT'])
 def update_task(task_id):
